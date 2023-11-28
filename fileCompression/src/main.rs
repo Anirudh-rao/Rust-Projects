6173286688dd500b3ef2d9ce70972ac86a1fae7c
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2023-11-28 20:59:45 +0530

    Changes

--- fileCompression/src/main.rs
+++ fileCompression/src/main.rs
@@ -1,5 +1,7 @@
+// Accessing External Crate
 extern crate flate2;
 
+//Accessing Flate libraries
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use std::env::args;
@@ -9,21 +11,34 @@ use std::io::BufReader;
 use std::time::Instant;
 
 
-fn main() {
-    if args().len() != 3 {
+fn main(){
+    // If arguments Length not equal to 3
+    //
+    if args().len() != 3{
         eprintln!("Usage: `source` `target`");
         return;
     }
-    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    //Select source file and get the 1st nth argument 
+    let mut input =  BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    // Target file as 2nd nth argument
     let output = File::create(args().nth(2).unwrap()).unwrap();
-    let mut encoder = GzEncoder::new(output, Compression::default());
-    let start = Instant::now();
+    // create an encoder 
+    let mut encoder GzEncoder::new(output, Compression::default());
+    //Start encoder- New instant
+    let start = Instant::new();
+    // copy data and encoder
     copy(&mut input, &mut encoder).unwrap();
     let output = encoder.finish().unwrap();
+    //Getting source lenght
     println!(
-        "Source len: {:?}",
-        input.get_ref().metadata().unwrap().len()
+        "Source len:{:?}",
+        input.get_ref().metadata().unwrap.len();
     );
-    println!("Target len: {:?}", output.metadata().unwrap().len());
-    println!("Elapsed: {:?}", start.elapsed());
+    //Getting the target lenght
+    println!(
+        "Target len:{:?}",
+        output.metadata().unwrap.len();
+    );
+    //Time Taken
+    println!("Elapsed time: {:?}", start.elapsed());
 }
