be65a2a0ffd5e92dcc6fffb87e7923cbde5181ca
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2023-11-28 21:04:38 +0530

    Compressin in Rust Completed

--- fileCompression/src/main.rs
+++ fileCompression/src/main.rs
@@ -1,7 +1,6 @@
-// Accessing External Crate
+
 extern crate flate2;
 
-//Accessing Flate libraries
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use std::env::args;
@@ -10,35 +9,21 @@ use std::io::copy;
 use std::io::BufReader;
 use std::time::Instant;
 
-
-fn main(){
-    // If arguments Length not equal to 3
-    //
-    if args().len() != 3{
+fn main() {
+    if args().len() != 3 {
         eprintln!("Usage: `source` `target`");
         return;
     }
-    //Select source file and get the 1st nth argument 
-    let mut input =  BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    // Target file as 2nd nth argument
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
     let output = File::create(args().nth(2).unwrap()).unwrap();
-    // create an encoder 
-    let mut encoder GzEncoder::new(output, Compression::default());
-    //Start encoder- New instant
-    let start = Instant::new();
-    // copy data and encoder
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
     copy(&mut input, &mut encoder).unwrap();
     let output = encoder.finish().unwrap();
-    //Getting source lenght
     println!(
-        "Source len:{:?}",
-        input.get_ref().metadata().unwrap.len();
+        "Source len: {:?}",
+        input.get_ref().metadata().unwrap().len()
     );
-    //Getting the target lenght
-    println!(
-        "Target len:{:?}",
-        output.metadata().unwrap.len();
-    );
-    //Time Taken
-    println!("Elapsed time: {:?}", start.elapsed());
+    println!("Target len: {:?}", output.metadata().unwrap().len());
+    println!("Elapsed: {:?}", start.elapsed());
 }
---
+++ fileCompression/target/debug/fileCompression.d
@@ -0,0 +1,1 @@
+D:\Rust\Rust-Projects\fileCompression\target\debug\fileCompression.exe: D:\Rust\Rust-Projects\fileCompression\src\main.rs
